05f57b9938cf8c060aacd8f04398a4e15ef39c6e
najia <najiaknizi199@gmail.com>
2024-06-06 11:50:56 +0100

    creation table film

--- movieSeries-app/src/main/java/com/dao/Main.java
+++ movieSeries-app/src/main/java/com/dao/Main.java
@@ -1,11 +1,18 @@
 package com.dao;
 
+import com.model.Admin;
 import com.model.Favorie;
 import com.model.Users;
+import com.model.film;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+
+import java.io.File;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
@@ -17,18 +24,43 @@ public class Main {
 
 
         try {
-            Users user = session.get(Users.class,1);
+
             // Create a user object
-            Favorie favorie = new Favorie();
-            favorie.setUser(user);
+            film films = new film();
+            films.setActeur("Acterur film1");
+            films.setCreateur("createurFim1");
+
+            DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+            Date dateDebut = dateFormat.parse("2020/02/02");
+            films.setDate_debut(dateDebut);
 
 
-            // Start a transaction
+            DateFormat dateFormat2 = new SimpleDateFormat("yyyy/MM/dd");
+            Date datefin = dateFormat2.parse("2024/04/04");
+            films.setDate_fin(datefin);
+
+            films.setDescription("film_description");
+
+            films.setGenre("genre film1");
+            films.setImage("profile");
+
+            films.setNote("note1");
+            films.setSaison("filmSaison");
+            films.setText("filmText");
+
+
+            Admin admin = session.get(Admin.class,1);
+            films.setAdmin(admin);
+            Favorie favorie = session.get(Favorie.class,1);
+            films.setFavorie(favorie);
+
+
+
             Transaction transaction = session.beginTransaction();
 
 
             // Save the user object
-            session.save(favorie);
+            session.save(films);
 
             // Commit transaction
             transaction.commit();
